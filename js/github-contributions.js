84458d637d53e8e4cbc26273e1914966b99c5ad6
ktnd111 <barrelc12@gmail.com>
2025-03-17 18:15:33 +0900

    相対パスを使用してコントリビューションデータのJSONファイルを取得するよう変更

--- js/github-contributions.js
+++ js/github-contributions.js
@@ -7,8 +7,8 @@ const USERNAME = 'ktnd111';
 async function fetchContributionData() {
     console.log('fetchContributionData: 開始');
     try {
-        // GitHubページ用の絶対パス
-        const path = '/ktnd111.github.io/data/contributions.json';
+        // 相対パスを使用してJSONファイルを取得
+        const path = '../data/contributions.json';
         console.log(`JSONファイルを取得試行: ${path}`);
         
         const response = await fetch(path);
